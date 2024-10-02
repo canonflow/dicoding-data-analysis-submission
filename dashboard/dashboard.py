a1cdab09771452586af6d27fbaea7c2c38372513
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-10-03 00:06:28 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -103,13 +103,24 @@ min_date = all_df['order_purchase_timestamp'].min()
 max_date = all_df['order_purchase_timestamp'].max()
 
 with st.sidebar:
-    start_date, end_date = st.date_input(
-        label="Rentang Waktu",
+    st.markdown("""
+    # Interactive Dashboard
+    Anda dapat memilih range tanggal di bawah ini
+    
+    """)
+    input_date= st.date_input(
+        label="Rentang Tanggal",
         min_value=min_date,
         max_value=max_date,
         value=[min_date, max_date]
     )
-
+    if len(input_date) == 2:
+        start_date, end_date = input_date
+        st.success(f"Range tanggal yang dipilih dari {start_date} sampai {end_date}")
+    else:
+        st.error("Mohon pilih rantang tanggal akhirnya!")
+try:
+    # ===== SETUP DATA =====
     main_df = all_df[(all_df["order_purchase_timestamp"] >= str(start_date)) & (all_df["order_purchase_timestamp"] <= str(end_date))]
     sum_order_items_df = create_sum_order_items_df(main_df)
     ratings = create_ratings(main_df)
@@ -120,165 +131,178 @@ with st.sidebar:
         "customer_id": "nunique",
     })
 
-st.header("Brazilian E-Commerce")
+    st.header("🛒 Brazilian E-Commerce Dashboard")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric("Total Order", value=f'{main_df["order_id"].nunique():,}')
+    with col2:
+        st.metric("Total Customer", value=f'{main_df["customer_id"].nunique():,}')
 
-# ===== Number 1 GOES HERE =====
-st.subheader("Best and Worst Performing Product by Number of Order")
+    st.metric("Total Revenue", value=format_currency(main_df["price"].sum(), 'BRL', locale='pt_BR'))
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 9))
-colors = ["#E46A76", "#FAD4D8", "#FAD4D8", "#FAD4D8", "#FAD4D8"]
+    # ===== Number 1 GOES HERE =====
+    st.subheader("Best and Worst Performing Product by Number of Order")
 
-sns.barplot(
-    x="order_id",
-    y="product_category_name_english",
-    data=sum_order_items_df.head(5),
-    palette=colors,
-    ax=ax[0]
-)
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 9))
+    colors = ["#E46A76", "#FAD4D8", "#FAD4D8", "#FAD4D8", "#FAD4D8"]
 
-ax[0].set_ylabel(None)
-ax[0].set_xlabel(None)
-ax[0].set_title("Best Performing Product", loc="center", fontsize=20)
-ax[0].tick_params(axis="y", labelsize=18)
+    sns.barplot(
+        x="order_id",
+        y="product_category_name_english",
+        data=sum_order_items_df.head(5),
+        palette=colors,
+        ax=ax[0]
+    )
 
-sns.barplot(
-    x="order_id",
-    y="product_category_name_english",
-    data=sum_order_items_df.sort_values(by="order_id", ascending=True).head(5),
-    palette=colors,
-    ax=ax[1]
-)
+    ax[0].set_ylabel(None)
+    ax[0].set_xlabel(None)
+    ax[0].set_title("Best Performing Product", loc="center", fontsize=20)
+    ax[0].tick_params(axis="y", labelsize=18)
 
-ax[1].set_ylabel(None)
-ax[1].set_xlabel(None)
-ax[1].invert_xaxis()
-ax[1].yaxis.set_label_position("right")
-ax[1].yaxis.tick_right()
-ax[1].set_title("Worst Performing Product", loc="center", fontsize=20)
-ax[1].tick_params(axis="y", labelsize=18)
-plt.tight_layout()
-st.pyplot(fig)
-# ===== Number 1 END HERE =====
+    sns.barplot(
+        x="order_id",
+        y="product_category_name_english",
+        data=sum_order_items_df.sort_values(by="order_id", ascending=True).head(5),
+        palette=colors,
+        ax=ax[1]
+    )
 
-# ===== Number 2 GOES HERE =====
-st.subheader("Customer Ratings")
-max_ratings = ratings.idxmax()
-colors = ["#FAD4D8", "#FAD4D8", "#E46A76", "#FAD4D8", "#FAD4D8"]
-fig, ax = plt.subplots(figsize=(10, 5))
+    ax[1].set_ylabel(None)
+    ax[1].set_xlabel(None)
+    ax[1].invert_xaxis()
+    ax[1].yaxis.set_label_position("right")
+    ax[1].yaxis.tick_right()
+    ax[1].set_title("Worst Performing Product", loc="center", fontsize=20)
+    ax[1].tick_params(axis="y", labelsize=18)
+    plt.tight_layout()
+    st.pyplot(fig)
+    # ===== Number 1 END HERE =====
 
-sns.barplot(
-    x=ratings.index,
-    y=ratings.values,
-    palette=['#E46A76' if score == max_ratings else "#FAD4D8" for score in ratings.index],
-    order=ratings.index,
-    ax=ax
-)
+    # ===== Number 2 GOES HERE =====
+    st.subheader("Customer Ratings")
+    max_ratings = ratings.idxmax()
+    colors = ["#FAD4D8", "#FAD4D8", "#E46A76", "#FAD4D8", "#FAD4D8"]
+    fig, ax = plt.subplots(figsize=(10, 5))
 
-ax.set_title("Rating Customers by E-Commerce Service", loc="center", fontsize=20)
-ax.set_ylabel("Count", fontsize=16)
-ax.set_xlabel("Rating", fontsize=14)
-plt.tight_layout()
-st.pyplot(fig)
-# ===== Number 2 END HERE =====
+    sns.barplot(
+        x=ratings.index,
+        y=ratings.values,
+        palette=['#E46A76' if score == max_ratings else "#FAD4D8" for score in ratings.index],
+        order=ratings.index,
+        ax=ax
+    )
 
-# ===== Number 3 and 4 GOES HERE =====
-st.subheader("Order and Revenue")
-fig, ax = plt.subplots(figsize=(12, 7), nrows=2, ncols=1)
-ax[0].plot(
-    monthly_orders_df["order_approved_at"],
-    monthly_orders_df["order_count"],
-    marker='o',
-    linewidth=2,
-    color="#E46A76"
-)
-ax[0].set_title("Number of Orders per Month", loc="center", fontsize=20)
-ax[0].set_xlabel("", labelpad=10)
-ax[0].tick_params(axis="x", labelsize=12, labelrotation=45)
-ax[0].tick_params(axis='y', labelsize=10)
-ax[0].grid()
+    ax.set_title("Rating Customers by E-Commerce Service", loc="center", fontsize=20)
+    ax.set_ylabel("Count", fontsize=16)
+    ax.set_xlabel("Rating", fontsize=14)
+    plt.tight_layout()
+    st.pyplot(fig)
+    # ===== Number 2 END HERE =====
 
-ax[1].plot(
-    monthly_revenue_df["order_approved_at"],
-    monthly_revenue_df["revenue"],
-    marker='o',
-    linewidth=2,
-    color="#E46A76"
-)
-ax[1].set_title("Number of Orders per Month", loc="center", fontsize=20)
-ax[1].set_xlabel("", labelpad=10)
-ax[1].tick_params(axis="x", labelsize=12, labelrotation=45)
-ax[1].tick_params(axis='y', labelsize=10)
+    # ===== Number 3 and 4 GOES HERE =====
+    st.subheader("Order and Revenue")
+    fig, ax = plt.subplots(figsize=(12, 7), nrows=2, ncols=1)
+    ax[0].plot(
+        monthly_orders_df["order_approved_at"],
+        monthly_orders_df["order_count"],
+        marker='o',
+        linewidth=2,
+        color="#E46A76"
+    )
+    ax[0].set_title("Number of Orders per Month", loc="center", fontsize=20)
+    ax[0].set_xlabel("", labelpad=10)
+    ax[0].tick_params(axis="x", labelsize=12, labelrotation=45)
+    ax[0].tick_params(axis='y', labelsize=10)
+    ax[0].grid()
 
-plt.tight_layout()
-plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
-ax[1].grid()
-st.pyplot(fig)
-# ===== Number 3 and 4 END HERE =====
+    ax[1].plot(
+        monthly_revenue_df["order_approved_at"],
+        monthly_revenue_df["revenue"],
+        marker='o',
+        linewidth=2,
+        color="#E46A76"
+    )
+    ax[1].set_title("Number of Orders per Month", loc="center", fontsize=20)
+    ax[1].set_xlabel("", labelpad=10)
+    ax[1].tick_params(axis="x", labelsize=12, labelrotation=45)
+    ax[1].tick_params(axis='y', labelsize=10)
 
-# ===== RFM GOES HERE =====
-st.subheader("RFM Analysis")
-fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(30, 20))
+    plt.tight_layout()
+    plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    ax[1].grid()
+    st.pyplot(fig)
+    # ===== Number 3 and 4 END HERE =====
 
-sns.histplot(
-    rfm_df['recency'],
-    bins=50,
-    ax=ax[0],
-    kde=True,
-    color="#E46A76"
-).set_title('Recency Distribution', fontsize=32)
-ax[0].tick_params(axis="x", labelsize=18)
-ax[0].tick_params(axis='y', labelsize=15)
+    # ===== RFM GOES HERE =====
+    st.subheader("RFM Analysis")
+    fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(30, 20))
 
-sns.histplot(
-    rfm_df['frequency'],
-    bins=50,
-    ax=ax[1],
-    kde=True,
-    color="#E46A76"
-).set_title('Frequency Distribution' , fontsize=32)
+    sns.histplot(
+        rfm_df['recency'],
+        bins=50,
+        ax=ax[0],
+        kde=True,
+        color="#E46A76"
+    ).set_title('Recency Distribution', fontsize=32)
+    ax[0].tick_params(axis="x", labelsize=18)
+    ax[0].tick_params(axis='y', labelsize=15)
+    ax[0].grid()
 
-ax[1].tick_params(axis="x", labelsize=18)
-ax[1].tick_params(axis='y', labelsize=15)
+    sns.histplot(
+        rfm_df['frequency'],
+        bins=50,
+        ax=ax[1],
+        kde=True,
+        color="#E46A76"
+    ).set_title('Frequency Distribution' , fontsize=32)
 
-sns.histplot(
-    rfm_df['monetary'],
-    bins=50,
-    ax=ax[2],
-    kde=True,
-    color="#E46A76"
-).set_title('Monetary Distribution', fontsize=32)
+    ax[1].tick_params(axis="x", labelsize=18)
+    ax[1].tick_params(axis='y', labelsize=15)
+    ax[1].grid()
 
-ax[2].tick_params(axis="x", labelsize=18)
-ax[2].tick_params(axis='y', labelsize=15)
+    sns.histplot(
+        rfm_df['monetary'],
+        bins=50,
+        ax=ax[2],
+        kde=True,
+        color="#E46A76"
+    ).set_title('Monetary Distribution', fontsize=32)
 
-plt.tight_layout()
-st.pyplot(fig)
-# ===== RFM END HERE =====
+    ax[2].tick_params(axis="x", labelsize=18)
+    ax[2].tick_params(axis='y', labelsize=15)
+    ax[2].grid()
 
-# ===== Segmentation GOES HERE =====
-st.subheader("Customer Segmentations")
-categories = ["Champions", "Potential Loyalist", "Promising", "About To Sleep", "Lost"]
+    plt.tight_layout()
+    st.pyplot(fig)
+    # ===== RFM END HERE =====
 
-customer_segments.rename(columns={
-    "customer_id": "customer_count"
-}, inplace=True)
+    # ===== Segmentation GOES HERE =====
+    st.subheader("Customer Segmentations")
+    categories = ["Champions", "Potential Loyalist", "Promising", "About To Sleep", "Lost"]
 
-customer_segments["customer_segmentation"] = pd.Categorical(customer_segments["customer_segmentation"], categories)
-colors = ["#FAD4D8", "#FAD4D8", "#E46A76", "#FAD4D8", "#FAD4D8"]
+    customer_segments.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
 
-fig, ax = plt.subplots(figsize=(10, 5))
-sns.barplot(
-    x="customer_count",
-    y="customer_segmentation",
-    data=customer_segments.sort_values(by="customer_count", ascending=False),
-    palette=colors,
-    ax=ax
-)
+    customer_segments["customer_segmentation"] = pd.Categorical(customer_segments["customer_segmentation"], categories)
+    colors = ["#FAD4D8", "#FAD4D8", "#E46A76", "#FAD4D8", "#FAD4D8"]
 
-ax.set_title("Number of Customer by Segmentation", loc="center", fontsize=15)
-ax.set_ylabel(None)
-ax.set_xlabel(None)
-ax.tick_params(axis='y', labelsize=12)
-plt.tight_layout()
-st.pyplot(fig)
-# ===== Segmentation END HERE =====
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.barplot(
+        x="customer_count",
+        y="customer_segmentation",
+        data=customer_segments.sort_values(by="customer_count", ascending=False),
+        palette=colors,
+        ax=ax
+    )
+
+    ax.set_title("Number of Customer by Segmentation", loc="center", fontsize=15)
+    ax.set_ylabel(None)
+    ax.set_xlabel(None)
+    ax.tick_params(axis='y', labelsize=12)
+    plt.tight_layout()
+    st.pyplot(fig)
+    # ===== Segmentation END HERE =====
+except Exception as ex:
+    st.error(ex)
+    st.error(f"Pilih tanggal mulai dan akhir!")
